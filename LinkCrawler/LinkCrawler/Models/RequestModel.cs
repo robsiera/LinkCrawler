f5a5b0d4a54bc223a88f97aa613fcb86937bd4b2
holoncom <github.rsiera@holoncom.eu>
2020-10-19 11:11:04 +0200

    remove obsolete code

--- LinkCrawler/LinkCrawler/Models/RequestModel.cs
+++ LinkCrawler/LinkCrawler/Models/RequestModel.cs
@@ -1,20 +1,16 @@
-﻿using RestSharp;
-
-namespace LinkCrawler.Models
+﻿namespace LinkCrawler.Models
 {
     public class RequestModel
     {
         public readonly string Url;
         public readonly string ReferrerUrl;
         public bool IsInternalUrl { get; }
-        public RestClient Client;
 
         public RequestModel(string url, string referrerUrl, string baseUrl)
         {
             Url = url;
             IsInternalUrl = url.StartsWith(baseUrl);
             ReferrerUrl = referrerUrl;
-            Client = new RestClient(Url);
         }
     }
 }
